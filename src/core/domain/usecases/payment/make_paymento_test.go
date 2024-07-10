2ec0806bd82126208a353532babfc704c4737993
pspalmirasaraaf <pspalmirasaraaf@gmail.com>
2024-07-09 23:31:01 -0300

    add more cases

--- src/core/domain/usecases/payment/make_paymento_test.go
+++ src/core/domain/usecases/payment/make_paymento_test.go
@@ -1,19 +1,23 @@
 package usecases
 
 import (
+	"errors"
 	"github.com/CAVAh/api-tech-challenge/src/adapters/gateways/mocks"
 	"github.com/CAVAh/api-tech-challenge/src/core/domain/entities"
 	"github.com/CAVAh/api-tech-challenge/src/core/domain/enums"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestMakePaymentUseCase_FindByQrCode_Success(t *testing.T) {
+func TestMakePaymentUseCase_FindByQrCodeAndUpdatePayment_Success(t *testing.T) {
 	mockQrCode := "mockQrCode"
-	mockPayment := entities.Payment{PaymentStatus: enums.Paid, ID: 2, OrderID: 30}
+	mockPayment := entities.Payment{PaymentStatus: enums.AwaitingPayment, ID: 2, OrderID: 30}
 
 	prepare := func(t *testing.T, pr *mocks.PaymentRepository) {
 		t.Helper()
 		pr.On("FindByQrCode", mockQrCode).Return(mockPayment, nil)
+		pr.On("UpdateToPaid", mockPayment.ID).Return()
+
 	}
 
 	t.Run("when success findbyQRCode", func(t *testing.T) {
@@ -24,24 +28,25 @@ func TestMakePaymentUseCase_FindByQrCode_Success(t *testing.T) {
 			PaymentRepository: paymentRepositoryMock,
 		}
 
-		_, err := usecase.ExecuteWithQrCode(mockQrCode)
-		if err != nil {
-			return
-		}
+		output, err := usecase.ExecuteWithQrCode(mockQrCode)
 
+		assert.Equal(t, "Pago", output)
+		assert.Equal(t, nil, err)
 	})
 }
 
-func TestMakePaymentUseCase(t *testing.T) {
-	mockOrderId := uint(407)
+func TestMakePaymentUseCase_FindByQrCodeAndUpdatePayment_Fail(t *testing.T) {
+	mockQrCode := "mockQrCode"
 	mockPayment := entities.Payment{PaymentStatus: enums.Paid, ID: 2, OrderID: 30}
 
 	prepare := func(t *testing.T, pr *mocks.PaymentRepository) {
 		t.Helper()
-		pr.On("FindByOrderId", mockOrderId).Return(mockPayment, nil)
+		pr.On("FindByQrCode", mockQrCode).Return(mockPayment, nil)
+		pr.On("UpdateToPaid", mockPayment.ID).Return()
+
 	}
 
-	t.Run("when sucess findbyQRCode", func(t *testing.T) {
+	t.Run("when fails findbyQRCode", func(t *testing.T) {
 		paymentRepositoryMock := &mocks.PaymentRepository{}
 		prepare(t, paymentRepositoryMock)
 
@@ -49,9 +54,61 @@ func TestMakePaymentUseCase(t *testing.T) {
 			PaymentRepository: paymentRepositoryMock,
 		}
 
-		_, err := usecase.ExecuteWithOrderId(mockOrderId)
-		if err != nil {
-			return
+		output, err := usecase.ExecuteWithQrCode(mockQrCode)
+
+		assert.Equal(t, output, "")
+		assert.Equal(t, errors.New("não foi possível efetuar o pagamento: o pagamento já foi pago"), err)
+	})
+}
+
+func TestMakePaymentUseCase_FindByOrderIdAndUpdatePayment_Success(t *testing.T) {
+	mockOrderId := uint(407)
+	mockPayment := entities.Payment{PaymentStatus: enums.AwaitingPayment, ID: 2, OrderID: 30}
+
+	prepare := func(t *testing.T, pr *mocks.PaymentRepository) {
+		t.Helper()
+		pr.On("FindByOrderId", mockOrderId).Return(mockPayment, nil)
+		pr.On("UpdateToPaid", mockPayment.ID).Return()
+
+	}
+
+	t.Run("when success FindByOrderId", func(t *testing.T) {
+		paymentRepositoryMock := &mocks.PaymentRepository{}
+		prepare(t, paymentRepositoryMock)
+
+		usecase := MakePaymentUseCase{
+			PaymentRepository: paymentRepositoryMock,
 		}
+
+		output, err := usecase.ExecuteWithOrderId(mockOrderId)
+
+		assert.Equal(t, "Pago", output)
+		assert.Equal(t, nil, err)
+	})
+}
+
+func TestMakePaymentUseCase_FindByOrderIdAndUpdatePayment_Fail(t *testing.T) {
+	mockOrderId := uint(407)
+	mockPayment := entities.Payment{PaymentStatus: enums.Paid, ID: 2, OrderID: 30}
+
+	prepare := func(t *testing.T, pr *mocks.PaymentRepository) {
+		t.Helper()
+		pr.On("FindByOrderId", mockOrderId).Return(mockPayment, nil)
+		pr.On("UpdateToPaid", mockPayment.ID).Return()
+
+	}
+
+	t.Run("when fails findbyQROrderId", func(t *testing.T) {
+		paymentRepositoryMock := &mocks.PaymentRepository{}
+		prepare(t, paymentRepositoryMock)
+
+		usecase := MakePaymentUseCase{
+			PaymentRepository: paymentRepositoryMock,
+		}
+
+		output, err := usecase.ExecuteWithOrderId(mockOrderId)
+
+		assert.Equal(t, output, "")
+		assert.Equal(t, errors.New("não foi possível efetuar o pagamento: o pagamento já foi pago"), err)
 	})
 }
