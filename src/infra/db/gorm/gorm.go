e1f8b4090261e3968e9e8bd21cbdde8328366615
pspalmirasaraaf <pspalmirasaraaf@gmail.com>
2024-07-12 22:56:55 -0300

    fix gorm

--- src/infra/db/gorm/gorm.go
+++ src/infra/db/gorm/gorm.go
@@ -1,10 +1,12 @@
 package gorm
 
 import (
+	"fmt"
 	"github.com/CAVAh/api-tech-challenge/src/infra/db/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 var (
@@ -14,11 +16,12 @@ var (
 
 func ConnectDB() {
 	/*
-	conectionString := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable TimeZone=America/Fortaleza"
-	DB, err = gorm.Open(postgres.Open(conectionString))
+		conectionString := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable TimeZone=America/Fortaleza"
+		DB, err = gorm.Open(postgres.Open(conectionString))
 	*/
 	conectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=America/Fortaleza",
-	os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	DB, err = gorm.Open(postgres.Open(conectionString))
 
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
