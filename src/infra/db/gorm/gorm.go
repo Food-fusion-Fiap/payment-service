563ed785dfe83b2ea3a17c9a214128cc7b86d7e1
pspalmirasaraaf <pspalmirasaraaf@gmail.com>
2024-07-14 17:28:39 -0300

    add printf to string

--- src/infra/db/gorm/gorm.go
+++ src/infra/db/gorm/gorm.go
@@ -21,6 +21,7 @@ func ConnectDB() {
 	*/
 	conectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=America/Fortaleza",
 		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	fmt.printf(conectionString)
 	DB, err = gorm.Open(postgres.Open(conectionString))
 
 	if err != nil {
