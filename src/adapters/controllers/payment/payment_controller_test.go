9f3b89cbb73a0dca4d2181e8f0e4241335384721
pspalmirasara <palmira.sara@unico.io>
2024-07-15 17:55:39 -0300

    more tests to controller

--- src/adapters/controllers/payment/payment_controller_test.go
+++ src/adapters/controllers/payment/payment_controller_test.go
@@ -10,8 +10,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
+	"github.com/CAVAh/api-tech-challenge/src/core/domain/usecases/check_payment_status"
 	"github.com/CAVAh/api-tech-challenge/src/core/domain/usecases/create_qr_code"
 	"github.com/CAVAh/api-tech-challenge/src/core/domain/usecases/get_all_payments"
+	"github.com/CAVAh/api-tech-challenge/src/core/domain/usecases/make_payment"
 )
 
 func TestRequestQrCode_Success(t *testing.T) {
@@ -34,26 +36,6 @@ func TestRequestQrCode_Success(t *testing.T) {
 	assert.Equal(t, http.StatusOK, w.Code)
 }
 
-//func TestPay_Success(t *testing.T) {
-//	gin.SetMode(gin.TestMode)
-//
-//	useCaseMock := &make_payment.MakePaymentInterfaceMock{}
-//	useCaseMock.On("ExecuteCreateQrCode", mock.Anything).Return("mockQrCode", nil)
-//
-//	r := gin.Default()
-//	r.GET("/payments/qr-code", func(c *gin.Context) {
-//		RequestQrCode(c, useCaseMock)
-//	})
-//
-//	req, _ := http.NewRequest(http.MethodGet, "/payments/qr-code", nil)
-//	req.Header.Set("Content-Type", "application/json")
-//	w := httptest.NewRecorder()
-//
-//	r.ServeHTTP(w, req)
-//
-//	assert.Equal(t, http.StatusOK, w.Code)
-//}
-
 func TestRequestQrCode_Fails(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 
@@ -66,6 +48,126 @@ func TestRequestQrCode_Fails(t *testing.T) {
 	})
 
 	req, _ := http.NewRequest(http.MethodGet, "/payments/qr-code", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestPay_Success(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	useCaseMock := &make_payment.MakePaymentInterfaceMock{}
+	useCaseMock.On("ExecuteWithOrderId", mock.Anything).Return("success", nil)
+
+	r := gin.Default()
+	r.POST("/payments/alternative-pay", func(c *gin.Context) {
+		Pay(c, useCaseMock)
+	})
+
+	req, _ := http.NewRequest(http.MethodPost, "/payments/alternative-pay", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestPay_Fails(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	useCaseMock := &make_payment.MakePaymentInterfaceMock{}
+	useCaseMock.On("ExecuteWithOrderId", mock.Anything).Return("", errors.New("some error"))
+
+	r := gin.Default()
+	r.POST("/payments/alternative-pay", func(c *gin.Context) {
+		Pay(c, useCaseMock)
+	})
+
+	req, _ := http.NewRequest(http.MethodPost, "/payments/alternative-pay", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestPayQrCode_Success(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	useCaseMock := &make_payment.MakePaymentInterfaceMock{}
+	useCaseMock.On("ExecuteWithQrCode", mock.Anything).Return("success", nil)
+
+	r := gin.Default()
+	r.POST("/payments/alternative-pay-with-qr-code", func(c *gin.Context) {
+		PayQrCode(c, useCaseMock)
+	})
+
+	req, _ := http.NewRequest(http.MethodPost, "/payments/alternative-pay-with-qr-code", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestPayQrCode_Fails(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	useCaseMock := &make_payment.MakePaymentInterfaceMock{}
+	useCaseMock.On("ExecuteWithQrCode", mock.Anything).Return("", errors.New("some error"))
+
+	r := gin.Default()
+	r.POST("/payments/alternative-pay-with-qr-code", func(c *gin.Context) {
+		PayQrCode(c, useCaseMock)
+	})
+
+	req, _ := http.NewRequest(http.MethodPost, "/payments/alternative-pay-with-qr-code", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestCheckOrderPaymentStatus_Success(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	useCaseMock := &check_payment_status.CheckPaymentStatusUseCaseInterfaceMock{}
+	useCaseMock.On("ExecuteCheckPaymentStatus", mock.Anything).Return("success", nil)
+
+	r := gin.Default()
+	r.GET("/payments/status", func(c *gin.Context) {
+		CheckOrderPaymentStatus(c, useCaseMock)
+	})
+
+	req, _ := http.NewRequest(http.MethodGet, "/payments/status", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestCheckOrderPaymentStatus_Fails(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	useCaseMock := &check_payment_status.CheckPaymentStatusUseCaseInterfaceMock{}
+	useCaseMock.On("ExecuteCheckPaymentStatus", mock.Anything).Return("", errors.New("some error"))
+
+	r := gin.Default()
+	r.GET("/payments/status", func(c *gin.Context) {
+		CheckOrderPaymentStatus(c, useCaseMock)
+	})
+
+	req, _ := http.NewRequest(http.MethodGet, "/payments/status", nil)
 	req.Header.Set("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 
